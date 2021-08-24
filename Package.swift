f60e54e917a9413e7fa5bbdbb002677c730e0b63
Brian Strobach <brian@appsaurus.io>
2021-08-24 13:17:46 -0500

    Cleanup

--- Package.swift
+++ Package.swift
@@ -1,29 +1,6 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-//let package = Package(
-//    name: "VaporExtensions",
-//    products: [
-//        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-//        .library(
-//            name: "VaporExtensions",
-//            targets: ["VaporExtensions"]),
-//    ],
-//    dependencies: [
-//		.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.0")),
-//    ],
-//    targets: [
-//        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-//        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-//        .target(
-//            name: "VaporExtensions",
-//            dependencies: ["Vapor"]),
-//        .testTarget(
-//            name: "VaporExtensionsTests",
-//            dependencies: ["VaporExtensions"]),
-//    ]
-//)
-
 import PackageDescription
 
 let package = Package(
