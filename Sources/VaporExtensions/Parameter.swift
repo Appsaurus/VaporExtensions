a3ef1da3bfd52fe04e7f52300b5c38cfe65bf7c0
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2021-09-08 15:14:18 -0500

    Removing redundant implementation.

--- Sources/VaporExtensions/Parameter.swift
+++ Sources/VaporExtensions/Parameter.swift
@@ -33,20 +33,4 @@ public extension Parameters {
     func next<P>(_ parameter: P.Type) throws -> P.ResolvedParameter where P: Parameter {
         return try self.require(P.parameter, as: P.ResolvedParameter.self)
     }
-
-    func require(_ name: String) throws -> String {
-        return try self.require(name, as: String.self)
-    }
-
-    func require<T>(_ name: String, as type: T.Type = T.self) throws -> T where T: LosslessStringConvertible {
-        guard let stringValue: String = get(name) else {
-            throw Abort(.internalServerError)
-        }
-
-        guard let value = T.init(stringValue) else {
-            throw Abort(.unprocessableEntity)
-        }
-
-        return value
-    }
 }
