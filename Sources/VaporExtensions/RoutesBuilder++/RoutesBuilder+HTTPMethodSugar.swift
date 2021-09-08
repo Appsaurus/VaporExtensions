5b33b77695310a2930e76f9cc06ee26cd582f62b
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2021-09-08 14:18:02 -0500

    Cleanup formatting.

--- Sources/VaporExtensions/RoutesBuilder++/RoutesBuilder+HTTPMethodSugar.swift
+++ Sources/VaporExtensions/RoutesBuilder++/RoutesBuilder+HTTPMethodSugar.swift
@@ -8,53 +8,53 @@
 import Vapor
 
 public extension RoutesBuilder {
-
+    
     @discardableResult
     func get<R: ResponseEncodable>(_ path: PathComponentRepresentable...,
-                                          use closure: @escaping (Request) throws -> R) -> Route {
+                                   use closure: @escaping (Request) throws -> R) -> Route {
         on(.GET, path, use: closure)
-
+        
     }
-
+    
     @discardableResult
     func put<R: ResponseEncodable>(_ path: PathComponentRepresentable...,
-                                                        use closure: @escaping (Request) throws -> R) -> Route {
+                                   use closure: @escaping (Request) throws -> R) -> Route {
         on(.PUT, path, use: closure)
     }
-
+    
     @discardableResult
     func post<R: ResponseEncodable>(_ path: PathComponentRepresentable...,
-                                          use closure: @escaping (Request) throws -> R) -> Route {
+                                    use closure: @escaping (Request) throws -> R) -> Route {
         on(.POST, path, use: closure)
-
+        
     }
-
+    
     @discardableResult
     func patch<R: ResponseEncodable>(_ path: PathComponentRepresentable...,
-                                                        use closure: @escaping (Request) throws -> R) -> Route {
+                                     use closure: @escaping (Request) throws -> R) -> Route {
         on(.PATCH, path, use: closure)
     }
-
+    
     @discardableResult
     func delete<R: ResponseEncodable>(_ path: PathComponentRepresentable...,
-                                                        use closure: @escaping (Request) throws -> R) -> Route {
+                                      use closure: @escaping (Request) throws -> R) -> Route {
         on(.DELETE, path, use: closure)
     }
-
+    
     @discardableResult
     func on<R: ResponseEncodable>(_ method: HTTPMethod,
-                                         _ path: PathComponentRepresentable...,
-                                         use closure: @escaping (Request) throws -> R) -> Route {
+                                  _ path: PathComponentRepresentable...,
+                                  use closure: @escaping (Request) throws -> R) -> Route {
         on(method, path, use: closure)
-
+        
     }
-
+    
     @discardableResult
     func on<R: ResponseEncodable>(_ method: HTTPMethod,
-                                         _ path: [PathComponentRepresentable],
-                                         use closure: @escaping (Request) throws -> R) -> Route {
+                                  _ path: [PathComponentRepresentable],
+                                  use closure: @escaping (Request) throws -> R) -> Route {
         on(method, path.pathComponents, use: closure)
-
+        
     }
 }
 
--- Sources/VaporExtensions/RoutesBuilder++/RoutesBuilder+Parameters.swift
+++ Sources/VaporExtensions/RoutesBuilder++/RoutesBuilder+Parameters.swift
@@ -8,62 +8,62 @@
 import Vapor
 
 public extension RoutesBuilder {
-
+    
     @discardableResult
     func get<P: Parameter, R: ResponseEncodable>(_ path: PathComponentRepresentable...,
                                                  params: P.Type = P.self,
-                                          use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                 use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(.GET, path, params: params, use: closure)
-
+        
     }
-
+    
     @discardableResult
     func put<P: Parameter, R: ResponseEncodable>(_ path: PathComponentRepresentable...,
                                                  params: P.Type = P.self,
-                                                        use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                 use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(.PUT, path, params: params, use: closure)
     }
-
+    
     @discardableResult
     func post<P: Parameter, R: ResponseEncodable>(_ path: PathComponentRepresentable...,
                                                   params: P.Type = P.self,
-                                          use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                  use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(.POST, path, params: params, use: closure)
-
+        
     }
-
+    
     @discardableResult
     func patch<P: Parameter, R: ResponseEncodable>(_ path: PathComponentRepresentable...,
                                                    params: P.Type = P.self,
-                                                        use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                   use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(.PATCH, path, params: params, use: closure)
     }
-
+    
     @discardableResult
     func delete<P: Parameter, R: ResponseEncodable>(_ path: PathComponentRepresentable...,
                                                     params: P.Type = P.self,
-                                                        use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                    use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(.DELETE, path, params: params, use: closure)
     }
-
+    
     @discardableResult
     func on<P: Parameter, R: ResponseEncodable>(_ method: HTTPMethod,
-                                         _ path: PathComponentRepresentable...,
-                                         params: P.Type = P.self,
-                                         use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                _ path: PathComponentRepresentable...,
+                                                params: P.Type = P.self,
+                                                use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(method, path, params: params, use: closure)
-
+        
     }
-
+    
     @discardableResult
     func on<P: Parameter, R: ResponseEncodable>(_ method: HTTPMethod,
-                                         _ path: [PathComponentRepresentable],
-                                         params: P.Type = P.self,
-                                         use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
+                                                _ path: [PathComponentRepresentable],
+                                                params: P.Type = P.self,
+                                                use closure: @escaping (Request, P.ResolvedParameter) throws -> R) -> Route {
         on(method, path) { request -> R in
             let params = try request.parameters.next(P.self)
             return try closure(request, params)
         }
-
+        
     }
 }
