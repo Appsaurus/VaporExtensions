bc8b574142270707818c30640130365a25971758
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2021-10-14 14:09:45 -0500

    Future extensions for iterating over collections.

--- Sources/VaporExtensions/NIO++/Future++/Future++.swift
+++ Sources/VaporExtensions/NIO++/Future++/Future++.swift
@@ -52,3 +52,78 @@ public extension Collection {
         return flatten(on: eventLoopReferencing.eventLoop)
     }
 }
+
+
+public extension Future where Value: Collection {
+    func transformEach<R>(to type: R.Type = R.self, _ transform: @escaping (Value.Element) throws -> R) -> Future<[R]> {
+        self.flatMap { values -> Future<[R]> in
+            do {
+                let transformedValues = try values.compactMap { value in
+                    return try transform(value)
+                }
+                return self.future(transformedValues)
+            }
+            catch {
+                return self.fail(with: error)
+            }
+        }
+    }
+
+    func flatMapEach<R>(to type: R.Type = R.self, on eventLoop: EventLoop, _ futureTransform: @escaping (Value.Element) throws -> Future<R>, continueOnError: Bool = false) -> Future<[R]> {
+        return self.tryFlatMap { collection in
+            return collection.map { (element) -> Future<R>? in
+                if continueOnError {
+                    return try? futureTransform(element)
+                }
+                else {
+                    do {
+                        return try futureTransform(element)
+                    }
+                    catch {
+                        return eventLoop.fail(with: error)
+                    }
+
+                }
+            }.removeNils().flatten(on: eventLoop)
+        }
+    }
+
+    func transformEach<R>(to type: R.Type = R.self, _ transform: @escaping (Value.Element) throws -> R?) -> Future<[R]> {
+        return self.flatMapThrowing { try $0.map { try transform($0)}.removeNils() }
+    }
+
+    func flatMapEach<R>(to type: R.Type = R.self, on eventLoop: EventLoop, _ futureTransform: @escaping (Value.Element) throws -> Future<R>?) -> Future<[R]> {
+
+        return self.tryFlatMap { collection in
+            do {
+                return try collection.map { (element) -> Future<R>? in
+                    return try futureTransform(element)
+                }.removeNils().flatten(on: eventLoop)
+            }
+            catch {
+                return self.fail(with: error)
+            }
+        }
+
+    }
+}
+
+// MARK: Removing nils
+fileprivate protocol OptionalType {
+    associatedtype Wrapped
+    func map<U>(_ f: (Wrapped) throws -> U) rethrows -> U?
+}
+
+extension Optional: OptionalType {}
+
+fileprivate extension Sequence where Iterator.Element: OptionalType {
+    func removeNils() -> [Iterator.Element.Wrapped] {
+        var result: [Iterator.Element.Wrapped] = []
+        for element in self {
+            if let element = element.map({ $0 }) {
+                result.append(element)
+            }
+        }
+        return result
+    }
+}
