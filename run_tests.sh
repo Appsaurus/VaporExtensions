b43de940624a7e229767c64be6fd18e13d6565f1
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2022-10-21 12:53:34 -0500

    Updating dependencies. Swift 5.5 support.

--- run_tests.sh
+++ run_tests.sh
@@ -9,7 +9,7 @@ echo "💧  Exporting docker-machine env"
 eval "$(docker-machine env default)"
 
 echo "💧  Running unit tests on Linux"
-docker run -it -v $PWD:/root/code -w /root/code vapor/swift:5.4 /usr/bin/swift test --verbose
+docker run -it -v $PWD:/root/code -w /root/code vapor/swift:5.5 /usr/bin/swift test --verbose
 LINUX_EXIT=$?
 
 if [[ $MACOS_EXIT == 0 ]];
