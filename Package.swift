682fe79a38aae42cdfcea703f53961604f73b47a
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2025-04-15 15:25:04 -0500

    Updated depdendencies.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/Appsaurus/RoutingKitExtensions", exact: "1.0.1")
+        .package(url: "https://github.com/Appsaurus/RoutingKitExtensions", from: "1.0.2")
     ],
     targets: [
         .target(
